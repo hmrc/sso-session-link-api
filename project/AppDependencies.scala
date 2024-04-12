6176021e997af4b5de0817c91a639dac27bd63c0
ianmcnicholas <ian.mcnicholas@digital.hmrc.gov.uk>
2024-04-12 14:57:46 +0100

    GG-7608 : Upgrade to Play 3.0
    addressing platops-pr-bot-comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,13 @@ import sbt.*
 
 object AppDependencies {
   private val compile = Seq(
-    "uk.gov.hmrc"        %% "government-gateway-domain-play-30" % "9.0.0"
+    "uk.gov.hmrc" %% "government-gateway-domain-play-30" % "9.0.0"
   )
 
   private val test = Seq(
-    "org.mockito"       %% "mockito-scala-scalatest"         % "1.17.31"        % Test,
-    "uk.gov.hmrc" %% "government-gateway-test-play-30" % "6.0.0" % Test
+    "org.mockito" %% "mockito-scala-scalatest"         % "1.17.31" % Test,
+    "uk.gov.hmrc" %% "bootstrap-test-play-30"          % "8.5.0"   % Test,
+    "uk.gov.hmrc" %% "government-gateway-test-play-30" % "6.0.0"   % Test
       excludeAll (
       ExclusionRule("com.fasterxml.jackson.core", "jackson-databind"),
       ExclusionRule("com.fasterxml.jackson.core", "jackson-core"),
